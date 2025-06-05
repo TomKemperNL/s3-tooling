af34b501d030c85e398a6d03271ce6fe3ba5619b
Tom <tom.kemper@hu.nl>
2025-06-05 15:47:34 +0200

    Paginering voor graphql: prs, issues en comments

--- src/main/github-client.ts
+++ src/main/github-client.ts
@@ -10,7 +10,7 @@ export type RepoResponse = {
     url: string,
     created_at: string,
     updated_at: string,
-    organization: { login: string},
+    organization: { login: string },
     lastMemberCheck: Date
 }
 
@@ -38,7 +38,7 @@ export class GithubClient {
         this.#kit = new Octokit({
             auth: githubToken,
             userAgent: 'ict.hu.nl:S3-Tools:Dev',
-            log: console
+            // log: console
         });
     }
 
@@ -46,7 +46,7 @@ export class GithubClient {
         return this.#kit.request('GET /user').then(response => response.data);
     }
 
-    async getMembers(org, repo) : Promise<MemberResponse[]>{
+    async getMembers(org, repo): Promise<MemberResponse[]> {
         let response = await this.#kit.repos.listCollaborators({
             repo: repo,
             owner: org
@@ -54,7 +54,7 @@ export class GithubClient {
         return response.data;
     }
 
-    async listRepos(org) : Promise<RepoResponse[]> {        
+    async listRepos(org): Promise<RepoResponse[]> {
         let pagination = this.#kit.paginate.iterator(this.#kit.repos.listForOrg,
             {
                 per_page: 100,
@@ -68,5 +68,161 @@ export class GithubClient {
         }
 
         return repos;
+    }
+
+    async #fetchComments(issueId: string) {
+        let nextCursor = "";
+        let hasNextPage = true;
+        let comments = [];
+
+        while (hasNextPage) {
+            let response: any = await this.#kit.graphql(`
+                query listComments($issueId: ID!, $cursor: String!) {
+                  node(id: $issueId) {
+                    ... on Issue {
+                      comments(first: 100, after: $cursor) {
+                        totalCount
+                        pageInfo {
+                          hasNextPage
+                          endCursor
+                        }
+                        nodes {
+                          author { login }
+                          createdAt
+                          body
+                        }
+                      }
+                    }
+                  }
+                }`, {
+                issueId, cursor: nextCursor
+            });
+            comments = comments.concat(response.node.comments.nodes);
+            hasNextPage = response.node.comments.pageInfo.hasNextPage;
+            nextCursor = response.node.comments.pageInfo.endCursor;
+        }
+
+        return comments;
+    }
+
+
+    async listIssues(org, repo) {
+        let nextCursor = "";
+        let hasNextPage = true;
+        let issues = [];
+
+        while (hasNextPage) {
+            let response: any = await this.#kit.graphql(`
+                query listIssues($org: String!, $repo: String!, $cursor: String!) {
+                  repository(owner: $org, name:$repo){
+                    issues(first:50, after: $cursor){
+                      totalCount 
+                      pageInfo {
+                        hasNextPage
+                        endCursor
+                      }
+                      nodes{
+                        id
+                        title
+                        body
+                        createdAt
+                        author { login }
+                        comments(first:20) {
+                          totalCount
+                          pageInfo{
+                            hasNextPage
+                            endCursor            
+                          }
+                          nodes{
+                            author { login }
+                            createdAt
+                            body
+                          }
+                        }
+                      }
+                    }
+                  }
+                }`, {
+                org, repo, cursor: nextCursor
+            });
+            issues = issues.concat(response.repository.issues.nodes);
+            hasNextPage = response.repository.issues.pageInfo.hasNextPage;
+            nextCursor = response.repository.issues.pageInfo.endCursor;
+        }
+
+        let issuesWithLotsOfComments = issues.filter(issue => issue.comments.totalCount > 20);
+        for (let issue of issuesWithLotsOfComments) {
+            issue.comments.nodes = await this.#fetchComments(issue.id);
+        }
+
+        return issues.map(issue => ({
+            ...issue,
+            author: issue.author.login,
+            comments: issue.comments.nodes.map(comment => ({
+                ...comment,
+                author: comment.author.login
+            }))
+        })
+        );            
+    }
+
+    async listPullRequests(org, repo) {
+        let nextCursor = "";
+        let hasNextPage = true;
+        let pullRequests = [];
+
+        while (hasNextPage) {
+            let response: any = await this.#kit.graphql(`
+                query listPrs($org: String!, $repo: String!, $cursor: String!) {
+                  repository(owner: $org, name:$repo){
+                    pullRequests(first:50, after: $cursor){
+                      totalCount 
+                      pageInfo {
+                        hasNextPage
+                        endCursor
+                      }
+                      nodes{
+                        id
+                        title
+                        body
+                        createdAt
+                        author { login }
+                        comments(first:20) {
+                          totalCount
+                          pageInfo{
+                            hasNextPage
+                            endCursor            
+                          }
+                          nodes{
+                            author { login }
+                            createdAt
+                            body
+                          }
+                        }
+                      }
+                    }
+                  }
+                }`, {
+                org, repo, cursor: nextCursor
+            });
+            pullRequests = pullRequests.concat(response.repository.pullRequests.nodes);
+            hasNextPage = response.repository.pullRequests.pageInfo.hasNextPage;
+            nextCursor = response.repository.pullRequests.pageInfo.endCursor;
+        }
+
+        let pullRequestsWithLotsOfComments = pullRequests.filter(issue => issue.comments.totalCount > 20);
+        for (let pullRequest of pullRequestsWithLotsOfComments) {
+            pullRequest.comments.nodes = await this.#fetchComments(pullRequest.id);
+        }
+
+        return pullRequests.map(pullRequest => ({
+            ...pullRequest,
+            author: pullRequest.author.login,
+            comments: pullRequest.comments.nodes.map(comment => ({
+                ...comment,
+                author: comment.author.login
+            }))
+        })
+        );            
     }
 }
--- test/graphql.manual.ts
+++ test/graphql.manual.ts
@@ -1,106 +1,28 @@
-import { Octokit } from "@octokit/rest";
+import { GithubClient } from "../src/main/github-client";
 console.log("Using token", process.env.GITHUB_TOKEN);
 
-let client = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
-});
+let client = new GithubClient(process.env.GITHUB_TOKEN);
 
 async function main() {
-    let result = await client.graphql({
-        query: `
-            query {
-  repository(name:"sd-s2-project-spaid-s", owner:"HU-SD-S2-studenten-2425"){
-   	issues(first:45
-    ){
-      totalCount
-      pageInfo{
-        endCursor
-        hasNextPage
-      }
-      nodes{
-        author{
-          login
-        },
-        title,
-        url,
-        createdAt,
-        closedAt,
-        body,
-        state,
-        comments(first:5) {
-          totalCount
-          pageInfo{
-            endCursor
-            hasNextPage
-          }
-          nodes{
-            author { login },
-            body
-          }          
-        }
-      }     
+
+    let projects = ['sd-s2-project-1-team-1-taak',
+'sd-s2-project-all4positivity',
+'sd-s2-project-alt-f4']
+    for(let p of projects) {
+      console.log(p);
+      let result = await client.listIssues('HU-SD-S2-studenten-2425', p);      
+      console.log(result.length, "issues found");
+      console.log(result.reduce((acc, issue) => {
+        return acc + issue.comments.length;
+      }, 0));
+
+      
+      let prs = await client.listPullRequests('HU-SD-S2-studenten-2425', p);      
+      console.log(prs.length, "prs found");
+      console.log(prs.reduce((acc, pr) => {
+        return acc + pr.comments.length;
+      }, 0));
     }
-  }
-}
-    `});
-    console.log(result);
+    
 }
 main();
-
-
-
-
-let someotherquery = `
-{
-  organization(login: "HU-SD-S2-studenten-2425") {
-    projectsV2(first: 100) {
-      nodes {
-        title
-        number
-        items(first:10){
-          nodes{
-            type
-            content{
-              ... on PullRequest {
-                title
-                author { login }
-                body
-                prstate: state
-                commits {
-                  nodes{
-                    commit{
-                      author { name, email}
-                      additions
-                      deletions
-                      message
-                      
-                    }
-                  }
-                }
-              }
-              ... on Issue {
-                title
-                author { login }
-                body
-                issuestate: state
-                issueType{ name }
-                labels {
-                  nodes{
-                    name
-                  }
-                }
-                
-              }
-            }
-          }
-        }
-        views(first: 10){
-          nodes{
-            name
-            
-          }
-        }
-      }
-    }
-  }
-}`
