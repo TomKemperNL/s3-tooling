61ba429f4d624b662e5ee072af3114ca27d47494
Tom <tom.kemper@hu.nl>
2025-06-06 08:44:26 +0200

    Hmm, comments zijn tricky... wat willen we daarmee?

--- src/main/project-statistics.ts
+++ src/main/project-statistics.ts
@@ -1,51 +1,71 @@
-import { Issue, PullRequest } from "../shared";
+import { Issue, PullRequest, Comment } from "../shared";
 import { GroupedCollection, ExportingArray } from "./repository-statistics";
 
 export class ProjectStatistics {
-    groupByAuthor() : GroupedCollection<ProjectStatistics>{
+
+    
+    constructor(private issues: Issue[], private prs: PullRequest[], private comments: Comment[] = []) {
         
+    }
+
+    groupByAuthor() : GroupedCollection<ProjectStatistics>{
         let results = {};
-        for(let i of this.issues){
-            if(!results[i.author]){
-                results[i.author] = {
-                    issues: [i],
-                    prs: []
+        function addOrAppend(type, key, value){
+            if(!results[key]){
+                results[key] = {
+                    issues: [],
+                    prs: [],
+                    comments: []
                 }
-            }else{
-                results[i.author].issues.push(i);
+            }
+            results[key][type].push(value);            
+        }
+
+        for(let i of this.issues){
+            addOrAppend('issues', i.author, { ...i, comments: []});
+            for(let c of i.comments){
+                addOrAppend('comments', c.author, c);
             }
         }
         for(let pr of this.prs){
-            if(!results[pr.author]){
-                results[pr.author] = {
-                    issues: [],
-                    prs: [pr]
-                };
-            }else{
-                results[pr.author].prs.push(pr);
+            addOrAppend('prs', pr.author, { ...pr, comments: []});
+            for(let c of pr.comments){
+                addOrAppend('comments', c.author, c);
             }
         }
 
         for(let author of Object.keys(results)){
-            results[author] = new ProjectStatistics(results[author].issues, results[author].prs);
+            results[author] = new ProjectStatistics(
+                results[author].issues, 
+                results[author].prs,
+                results[author].comments
+            );
         }
         
         return new GroupedCollection<ProjectStatistics>(results);
     }
 
-    constructor(private issues: Issue[], private prs: PullRequest[]) {
-        
+    static #getLines(carrier: Issue | PullRequest){
+        let commentLines = carrier.comments
+            .map(c => c.body.split("\n").length)
+            .reduce((a,b)=> a+b, 0);
+        return carrier.body.split("\n").length + (carrier.title ? 1 : 0) + commentLines;
     }
 
     getLines() : { lines: number} {
         let issueStats = this.issues.reduce((acc, issue) => {
-            return {lines: acc.lines + issue.body.split("\n").length + 1 }
+            return {lines: acc.lines + ProjectStatistics.#getLines(issue)}
         }, {lines: 0});
         let prStats = this.prs.reduce((acc, pr) => {
-            return {lines: acc.lines + pr.body.split("\n").length + 1 }
+            return {lines: acc.lines + ProjectStatistics.#getLines(pr)}
         }, { lines: 0});
+
+        let commentStats = this.comments.reduce((acc, comment) => {
+            return {lines: acc.lines + comment.body.split('\n').length}
+        }, { lines: 0});
+
         let total = {
-            lines: issueStats.lines + prStats.lines
+            lines: issueStats.lines + prStats.lines + commentStats.lines
         };
         return total;   
     }
--- test/project-statistics.test.ts
+++ test/project-statistics.test.ts
@@ -19,11 +19,16 @@ test('CanSumLinesPerAuthor', {}, () => {
         body: 'This is a test request.\nWith a new line',
         createdAt: new Date(),
         comments: [
+            {
+                author: 'Bob',
+                body: "I want it now!\nAnd it better be good.",
+                createdAt: new Date()
+            }
         ]
     }]);
 
     let results = stats.groupByAuthor().map(s => s.getLines()).export();
 
     expect(results["Fred"].lines).toBe(3); //1 title, +2 body
-    expect(results["Bob"].lines).toBe(2); //1 title, +1 body
+    expect(results["Bob"].lines).toBe(4); //1 title, +1 body + 2 comments
 });
