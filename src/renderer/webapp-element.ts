f4940f6928d354f3c934c966cf70efc58a708db4
Tom <tom.kemper@hu.nl>
2025-08-14 12:39:17 +0200

    demo genoeg

--- src/renderer/webapp-element.ts
+++ src/renderer/webapp-element.ts
@@ -1,21 +1,49 @@
-import { css, html, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import { css, html, LitElement, PropertyValues } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { when } from "lit/directives/when.js";
 
 @customElement("webapp-element")
 export class WebAppElement extends LitElement {
-
-    
+    @property({ type: String })
+    username: string;
+        
     constructor() {
         super();
-
+        this.username = null; 
     }
 
     static styles = css`
         `;
 
+    refresh(){
+        return fetch('/auth/session').then(r => r.json()).then((data: any) => {
+            this.username = data.user ? data.user.username : null;
+        });  
+    }
+
+    protected firstUpdated(_changedProperties: PropertyValues): void {
+        this.refresh();
+    }
+
+    logout(){
+        return fetch('/auth/session', {
+            method: 'DELETE'          
+        }).then(() => {
+            this.refresh();
+        })      
+    }
+
     render() {
         return html`
-        Mah webapp
+        ${when(this.username, () => html`
+            Hello ${this.username}
+            <button @click=${this.logout}>Logout</button>
+        `, () => html`
+            Hello guest
+            <button @click=${() => window.location.href = '/auth/github'}>Login with GitHub</button>
+        `)}
+        
+        
         `
     }
 }
--- src/web-setup.ts
+++ src/web-setup.ts
@@ -2,16 +2,20 @@ import express from "express"
 import { promisify } from "util";
 import { S3App } from "./main/index";
 import * as fspath from "path";
-
-let expressApp = express();
-let apiRouter = express.Router();
-
-expressApp.use('/api', apiRouter);
-expressApp.use('/assets', express.static('dist/src/renderer/assets'));
-expressApp.get('/*spa', (req, res) => {    
-    res.sendFile(fspath.resolve(process.cwd(), 'dist', 'src', 'renderer', 'web.html'));
+var passport = require('passport');
+passport.serializeUser((user: any, done: any) => {
+    done(null, user);
 });
 
+passport.deserializeUser((user: any, done: any) => {
+    return done(null, user);
+});
+
+
+import { Strategy as GitHubStrategy } from 'passport-github2';
+
+
+let expressApp = express();
 const listen = promisify(expressApp.listen).bind(expressApp);
 
 const PORT = process.env.PORT || 3000;
@@ -48,6 +52,55 @@ export function get(path: string = '') {
 }
 
 export async function setupWebHandlers(app: S3App) {
+    
+    const session: any = require('express-session');
+    const SQLiteStore: any = require('connect-sqlite3')(session);
+    
+    passport.use(new GitHubStrategy({
+        clientID: process.env.OAUTH_GITHUB_ID,
+        clientSecret: process.env.OAUTH_GITHUB_SECRET,
+        callbackURL: process.env.OAUTH_GITHUB_CALLBACK
+
+      },
+      function(accessToken: string, refreshToken: string, profile: any, done: any) {
+        console.log('GitHub profile received:', profile);
+        
+        return done(null, { token: accessToken, id: profile.id, username: profile.username});
+      }
+    ));
+
+    let apiRouter = express.Router();
+
+    expressApp.use('/assets', express.static('dist/src/renderer/assets'));
+    expressApp.use(session({
+        store: new SQLiteStore({ db: 'sessions.sqlite3' }),
+        secret: 's3-tooling-secret??',
+        cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }
+    }));
+    expressApp.use(passport.session());
+    expressApp.get('/auth/session', (req, res) => {
+        res.send({
+            user: req.user || null,
+        });
+    });
+    expressApp.delete('/auth/session', (req, res) => {
+        req.logout((err: any) => {
+            if (err) {
+                return res.status(500).send('Logout failed');
+            }
+            res.sendStatus(201);
+        });
+    });
+    expressApp.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email' ] }));      
+    expressApp.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), function(req, res) {        
+        res.redirect('/');
+    });
+
+    expressApp.use('/api', apiRouter);
+    expressApp.get('{/*spa}', (req, res) => {    
+        res.sendFile(fspath.resolve(process.cwd(), 'dist', 'src', 'renderer', 'web.html'));
+    });
+
     let appAsAny = <any> app;
     for(let registeredParam of paramRegistry){
         for(let key of Object.keys(getRegistry)){
@@ -94,6 +147,8 @@ export async function setupWebHandlers(app: S3App) {
         });        
     }
     
+
+
     
     console.log(`Listening on port ${PORT}`);
     await listen(PORT);
