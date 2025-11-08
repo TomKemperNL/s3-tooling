a24026c638aff73efc38cc5cb0bd1275de417937
Tom <tom.kemper@hu.nl>
2025-11-08 15:45:33 +0100

    Ok, het hoofdprobleem was dat Access met 2 s'en is

--- test/manual/README.md
+++ test/manual/README.md
@@ -1,1 +1,11 @@
 Dit zijn gewoon un-maintained uitprobeerscriptjes. Dus ehm, sorry!
+
+Hoe draai ik ze?
+
+Allereerst in de root:
+```.\node_modules\.bin\tsc --watch```
+En dan bijv.
+```node .\dist\test\manual\manual.js```
+
+Dit zorgt er voor dat ie de .env files uit de huidige working directory pakt.
+
--- test/manual/manual.ts
+++ test/manual/manual.ts
@@ -7,7 +7,7 @@ require('@dotenvx/dotenvx').config({path: ['.dev.env', '.env']})
 import { writeFile } from "fs/promises";
 
 let settings : Settings = {
-    githubToken: process.env.ACCES_TOKEN,
+    githubToken: process.env.ACCESS_TOKEN,
     canvasToken: process.env.CANVAS_TOKEN,
     dataPath: 'C:/s3-tooling-data2',
     keepDB: true,
@@ -16,7 +16,6 @@ let settings : Settings = {
 }
 
 async function main(){
-
     let app = new S3App(settings);
     await app.init();
     console.time("start")
